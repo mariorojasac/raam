c35e10b5f950ac4433ee92840792dfa2aaf39dab
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-11-24 14:02:35 -0600

    wip: adds view functions and pantry model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Pantry(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    location = models.TextField(max_length=250)
