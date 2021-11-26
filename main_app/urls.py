7f474807fc5999fb5cb634e1de0e1143561b11c6
vernellmiller <nezeey@gmail.com>
2021-11-26 11:56:50 -0500

    wip: adds create, update and delete paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('pantries/', views.pantry_index, name='pantries'),
     path('pantries/<int:pk>/', views.pantry_detail, name='detail'),
-
+    path('pantries/create/', views.PantryCreate.as_view(), name='pantries_create'),
+    path('pantries/<int:pk>/update/', views.PantryUpdate.as_view(), name='pantries_update'),
+    path('pantries/<int:pk>/delete/', views.PantryDelete.as_view(), name='pantries_delete'),
 ]
