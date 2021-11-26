4af146d82c841b51c316805fc2c854327ccf655c
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-11-26 10:56:21 -0600

    wip: adds class based view funtions

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
 
 class Pantry(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     location = models.TextField(max_length=250)
+
+
+    def __str__(self):
+        return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Pantry
 
 
@@ -14,7 +15,19 @@ def pantry_index(request):
     return render(request, 'pantries/index.html', { 'pantries': pantries})
 
 def pantry_detail(request, pk):
-    pantry = Pantry.objects.get(pk=pk)
+    pantry = Pantry.objects.get(id=pk)
     return render(request, 'pantries/detail.html', {'pantry': pantry})
 
+class PantryCreate(CreateView):
+    model = Pantry
+    fields = ('name', 'location', 'description')
 
+
+class PantryUpdate(UpdateView):
+    model = Pantry
+    fields = ('name', 'location', 'description')
+
+
+class PantryDelete(DeleteView):
+    model = Pantry
+    success_url = '/pantries/'
