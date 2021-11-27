5a04c9ae3a12a707f00af3c60719ba17b244327a
vernellmiller <nezeey@gmail.com>
2021-11-26 19:45:40 -0500

    adds: add schedule path

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('pantries/create/', views.PantryCreate.as_view(), name='pantries_create'),
     path('pantries/<int:pk>/update/', views.PantryUpdate.as_view(), name='pantries_update'),
     path('pantries/<int:pk>/delete/', views.PantryDelete.as_view(), name='pantries_delete'),
+    path('pantries/<int:pk>/add_schedule/', views.add_schedule, name='add_schedule'),
 ]
