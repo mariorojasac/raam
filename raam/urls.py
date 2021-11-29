bf527002e359f43827a28d1e2f16d35edc50bb3b
vernellmiller <nezeey@gmail.com>
2021-11-29 18:35:54 -0500

    feat: adds auth signup paths

--- raam/urls.py
+++ raam/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
