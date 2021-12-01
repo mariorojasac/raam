c7514fb5d950e8e76717848d71713c1da00d9a38
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-12-01 18:59:29 -0500

    adding styling

--- raam/settings.py
+++ raam/settings.py
@@ -124,5 +124,4 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
